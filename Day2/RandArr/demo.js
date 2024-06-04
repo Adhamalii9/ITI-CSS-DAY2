e6bf0e0834ebbf90b69dcfcc6364ed361e71d5b0
Adhamalii9 <60005374+Adhamalii9@users.noreply.github.com>
2024-06-04 15:23:33 +0300

    Update demo.js

--- Day2/RandArr/demo.js
+++ Day2/RandArr/demo.js
@@ -3,8 +3,7 @@ var arr = [];
 for (let index = 0; index < 5; index++) {
     arr[index] = prompt("ENTER NAME "+(index +1) +" : ");
 }
-var min = 0;
-var max = 4;
+
 
 function twoRandIndexes(){
     var ind1 , ind2;
